9123b694d6efe36bbcfe702650570b8bc226c489
maria-rangwala <maria.rangwala.16@gmail.com>
2024-11-23 15:17:41 +0530

    header footer

---
+++ src/components/header.js
@@ -0,0 +1,31 @@
+"use client";
+import React, { useState} from 'react';
+import "./header.css";
+import { IoIosMenu } from "react-icons/io";
+import { RxCross2 } from "react-icons/rx";
+
+const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+    return (
+        <header>
+            <div className={`navbar ${!showMenu ? 'responsive' : ''}`} id='navbar'>
+            <div className='nav'>
+            <img src="legalseva.svg" alt="logo" />
+            <button onClick={() => setShowMenu(!showMenu)}>{showMenu ? (<IoIosMenu  color="black" size="2em" alt="Menu"/>) : (<RxCross2 color="black" size="2em" alt="Close"/>)}</button>
+            </div>
+            <div className="menu">
+            {!showMenu ?
+            (<ul type="none">
+                <li><a href="/" onClick={() => setShowMenu(!showMenu)}>Home</a></li>
+                <li><a href="/contact" onClick={() => setShowMenu(!showMenu)}>Contact Us</a></li>
+                <li id='login-btn'><a href="/login" onClick={() => setShowMenu(!showMenu)}>Login</a></li>
+                <li id='signup-btn'><a href="/signup" onClick={() => setShowMenu(!showMenu)}>Sign up</a></li>
+            </ul>) : null
+            }
+            </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/landingpg.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function landingpg() {
+  return (
+    <div className=''>
+      <div className='welcome-user'>
+          <h1>
+            Welcome to<br/>
+            LegalSeva
+          </h1><br/>
+          <p>
+            Connect with Legal Service Providers in India
+          </p>
+          <button></button>
+      </div>
+      <div className='call-img'>
+
+      </div>
+    </div>
+  )
+}
+
+export default landingpg;
+
